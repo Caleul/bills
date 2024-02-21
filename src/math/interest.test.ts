f4884e81c961152b0cc30480556668dd835f27c3
Caleul <50340360+Caleul@users.noreply.github.com>
2024-02-21 18:04:14 -0300

    Update interest.test.ts

--- src/math/interest.test.ts
+++ src/math/interest.test.ts
@@ -3,12 +3,12 @@ import { calculateInterest } from './interest'
 
 test('Calc total fine', () => {
     expect(
-        calculateInterest(100, 1)
-    ).toEqual(0.00033 * 100)
+        calculateInterest(1000, 1)
+    ).toBeCloseTo(0.3)
 
     expect(
-        calculateInterest(100, 5000)
-    ).toEqual(0.00033 * 100 * 5000)
+        calculateInterest(1000, 30)
+    ).toBeCloseTo(10)
 
     expect(
         calculateInterest(100, -5)
